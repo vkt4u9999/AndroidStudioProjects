1f715a0895e0e32b9d78473f19205d0b62a795f5
vkt4u9999 <76712099+vkt4u9999@users.noreply.github.com>
2021-02-02 14:50:17 +0300

    taxi app 1st step

---
+++ TaxiApp/app/src/main/java/com/vkt4u9999/taxiapp/ChooseModeActivity.java
@@ -0,0 +1,27 @@
+package com.vkt4u9999.taxiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class ChooseModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_mode);
+
+
+    }
+
+    public void goToDriverSignIn(View view) {
+        startActivity(new Intent(ChooseModeActivity.this, DriverSignInActivity.class));
+
+    }
+
+    public void goToPassengerSignIn(View view) {
+        startActivity(new Intent(ChooseModeActivity.this, PassengerSignInActivity.class));
+    }
+}
---
+++ TaxiApp/app/src/main/java/com/vkt4u9999/taxiapp/PassengerSignInActivity.java
@@ -0,0 +1,14 @@
+package com.vkt4u9999.taxiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PassengerSignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger_sign_in);
+    }
+}
