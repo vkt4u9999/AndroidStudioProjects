e8d1dfa763303cc414a08e701120c940603307db
vkt4u9999 <76712099+vkt4u9999@users.noreply.github.com>
2021-02-25 23:25:48 +0300

    some refactoring, inline func and higher order functions

--- TelegramClone/app/src/main/java/com/vkt4u9999/telegramclone/ui/fragments/SettingsFragment.kt
+++ TelegramClone/app/src/main/java/com/vkt4u9999/telegramclone/ui/fragments/SettingsFragment.kt
@@ -2,9 +2,11 @@ package com.vkt4u9999.telegramclone.ui.fragments
 
 import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.net.Uri
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import com.google.firebase.storage.StorageReference
 import com.squareup.picasso.Picasso
 import com.theartofdev.edmodo.cropper.CropImage
 import com.theartofdev.edmodo.cropper.CropImageView
@@ -65,26 +67,20 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK && data != null){
-            val uri= CropImage.getActivityResult(data).uri
-            val path= REF_STORAGE_ROOT.child(FOLDER_PROFILE_IMAGE).child(CURRENT_UID)
-            path.putFile(uri).addOnCompleteListener{task1 ->
-                if (task1.isSuccessful){
-                    path.downloadUrl.addOnCompleteListener {task2 ->
-                        if (task2.isSuccessful){
-                            val photoUrl= task2.result.toString()
-                            REF_DATABASE_ROOT.child(NODE_USERS).child(CURRENT_UID).child(
-                                CHILD_PHOTO_URL).setValue(photoUrl).addOnCompleteListener { task3 ->
-                                if (task3.isSuccessful){
-                                    settings_user_photo.downloadAndSetImage(photoUrl)
-                                    showToast(getString(R.string.toast_data_update))
-                                    USER.photoUrl= photoUrl
-                                }
-                            }
-                        }
-                    }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            val uri = CropImage.getActivityResult(data).uri
+            val path = REF_STORAGE_ROOT.child(FOLDER_PROFILE_IMAGE).child(CURRENT_UID)
+
+            putImageToStorage(uri, path) {
+                getUrlFromStorage(path) {
+                    putUrlToDatabase(it) {}
+                    settings_user_photo.downloadAndSetImage(it)
+                    showToast(getString(R.string.toast_data_update))
+                    USER.photoUrl= it
                 }
             }
         }
     }
+
+
 }
--- TelegramClone/app/src/main/java/com/vkt4u9999/telegramclone/utilits/appDatabaseHelper.kt
+++ TelegramClone/app/src/main/java/com/vkt4u9999/telegramclone/utilits/appDatabaseHelper.kt
@@ -1,5 +1,6 @@
 package com.vkt4u9999.telegramclone.utilits
 
+import android.net.Uri
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -10,29 +11,50 @@ import com.vkt4u9999.telegramclone.models.User
 lateinit var AUTH: FirebaseAuth
 lateinit var REF_DATABASE_ROOT: DatabaseReference
 lateinit var REF_STORAGE_ROOT: StorageReference
-lateinit var USER:User
-lateinit var CURRENT_UID:String
+lateinit var USER: User
+lateinit var CURRENT_UID: String
 
-const val NODE_USERS= "users"
+const val NODE_USERS = "users"
 const val NODE_USERNAMES = "usernames"
 
-const val FOLDER_PROFILE_IMAGE= "profile_image"
+const val FOLDER_PROFILE_IMAGE = "profile_image"
 
-const val CHILD_ID= "id"
-const val CHILD_PHONE= "phone"
-const val CHILD_USERNAME= "username"
-const val CHILD_FULLNAME= "fullname"
-const val CHILD_BIO= "bio"
-const val CHILD_PHOTO_URL= "photoUrl"
+const val CHILD_ID = "id"
+const val CHILD_PHONE = "phone"
+const val CHILD_USERNAME = "username"
+const val CHILD_FULLNAME = "fullname"
+const val CHILD_BIO = "bio"
+const val CHILD_PHOTO_URL = "photoUrl"
 
 
-fun initFirebase(){
-    AUTH= FirebaseAuth.getInstance()
-    REF_DATABASE_ROOT= FirebaseDatabase.getInstance().reference
-    REF_STORAGE_ROOT=FirebaseStorage.getInstance().reference
+fun initFirebase() {
+    AUTH = FirebaseAuth.getInstance()
+    REF_DATABASE_ROOT = FirebaseDatabase.getInstance().reference
+    REF_STORAGE_ROOT = FirebaseStorage.getInstance().reference
 
-    USER= User()
-    CURRENT_UID= AUTH.currentUser?.uid.toString()
+    USER = User()
+    CURRENT_UID = AUTH.currentUser?.uid.toString()
+}
+
+inline fun putUrlToDatabase(url: String, crossinline function: () -> Unit) {
+    REF_DATABASE_ROOT.child(NODE_USERS).child(CURRENT_UID).child(
+        CHILD_PHOTO_URL
+    ).setValue(url)
+        .addOnSuccessListener { function() }
+        .addOnFailureListener { showToast(it.message.toString()) }
+}
+
+inline fun getUrlFromStorage(path: StorageReference, crossinline function: (url: String) -> Unit) {
+    path.downloadUrl
+        .addOnSuccessListener { function(it.toString()) }
+        .addOnFailureListener { showToast(it.message.toString()) }
+}
+
+inline fun putImageToStorage(uri: Uri, path: StorageReference, crossinline function: () -> Unit) {
+
+    path.putFile(uri)
+        .addOnSuccessListener { function() }
+        .addOnFailureListener { showToast(it.message.toString()) }
 }
 
 
